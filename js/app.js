f546a7e89340e97aa76b2c09e4b95db1782b35b7
John Fawcett <jrf0110@gmail.com>
2012-06-05 13:12:36 -0500

    how to build an api

--- js/app.js
+++ js/app.js
@@ -2,12 +2,13 @@ var slides = impress();
 slides.init();
 
 var routes = {
-  "everything": function(){
+  "everything": function(slide){
     console.log("entering - everything");
+
   }
 , "intro-1": function(slide){
     // Animate the arrow
-    this.animateArrow = setTimeout(function(){ $(slide).find('.arrow').addClass('animate-arrow'); }, 4000);
+    // this.animateArrow = setTimeout(function(){ $(slide).find('.arrow').addClass('animate-arrow'); }, 6000);
   }
 }
 , exitRoutes = {
@@ -16,14 +17,14 @@ var routes = {
   }
 , "intro-1": function(slide){
     // Kill arrow animation
-    clearTimeout(this.animateArrow);
-    $(slide).find('.arrow').removeClass('animate-arrow');
+    // clearTimeout(this.animateArrow);
+    // $(slide).find('.arrow').removeClass('animate-arrow');
   }
 }, routeScope = {}; // shared scope between enter/exit routes
 for (var key in routes) routeScope[key] = {};
 
 $(document).on('impress:stepenter', function(e){
-  routes.everything();
+  routes.everything(e.target, e);
   var route = e.target.id;
   if (routes.hasOwnProperty(route)) routes[route].call(routeScope[route], e.target, e);
 });
