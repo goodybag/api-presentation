fdddb7cfb60bd06dd5e1980c228dca99fa801348
John Fawcett <jrf0110@gmail.com>
2012-06-05 08:14:11 -0500

    finished cube theme, started on what are apis

--- js/app.js
+++ js/app.js
@@ -1,5 +1,35 @@
 var slides = impress();
 slides.init();
-$(document).ready(function(){
-  // impress().init();
+
+var routes = {
+  "everything": function(){
+    console.log("entering - everything");
+  }
+, "intro-1": function(slide){
+    // Animate the arrow
+    this.animateArrow = setTimeout(function(){ $(slide).find('.arrow').addClass('animate-arrow'); }, 4000);
+  }
+}
+, exitRoutes = {
+  "everything": function(){
+    console.log("exiting - everything");
+  }
+, "intro-1": function(slide){
+    // Kill arrow animation
+    clearTimeout(this.animateArrow);
+    $(slide).find('.arrow').removeClass('animate-arrow');
+  }
+}, routeScope = {}; // shared scope between enter/exit routes
+for (var key in routes) routeScope[key] = {};
+
+$(document).on('impress:stepenter', function(e){
+  routes.everything();
+  var route = e.target.id;
+  if (routes.hasOwnProperty(route)) routes[route].call(routeScope[route], e.target, e);
+});
+
+$(document).on('impress:stepleave', function(e){
+  exitRoutes.everything();
+  var route = e.target.id;
+  if (exitRoutes.hasOwnProperty(route)) exitRoutes[route].call(routeScope[route], e.target, e);
 });
